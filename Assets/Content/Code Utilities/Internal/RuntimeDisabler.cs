0adb8938557623090922d14108eaf0cafdd09671
Tyler <Jdngray77@icloud.com>
2020-06-23 20:17:49 +0100

    Git Scene Message
    Created scene message (Tagged to not be included in builds)
    Added monobehaviour to disable

---
+++ Assets/Content/Code Utilities/Internal/RuntimeDisabler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Monobehaviour component to disable objects in the hierarchy at runtime</summary>
+public class RuntimeDisabler : MonoBehaviour
+{
+    void Start() => gameObject.SetActive(false);
+}
